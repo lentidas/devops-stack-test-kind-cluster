78bc795328857dd3725d768a6201ed5c7bcbf42f
lentidas <33546359+lentidas@users.noreply.github.com>
2023-08-15 16:27:44 +0200

    docs: add latest comment

--- locals.tf
+++ locals.tf
@@ -3,6 +3,6 @@ locals {
   cluster_name           = "gh-kind-cluster"
   base_domain            = format("%s.nip.io", replace(module.traefik.external_ip, ".", "-"))
   cluster_issuer         = "ca-issuer"
-  enable_service_monitor = false
+  enable_service_monitor = false # Can be enabled after the first bootstrap.
   app_autosync           = true ? { allow_empty = false, prune = true, self_heal = true } : {}
 }
