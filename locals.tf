6c820fa1b7c4875fed317c1aed7ee4da4a29dce5
lentidas <33546359+lentidas@users.noreply.github.com>
2023-09-08 14:25:07 +0200

    feat: update Kubernetes version

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  kubernetes_version     = "v1.27.3"
+  kubernetes_version     = "v1.28.0"
   cluster_name           = "gh-kind-cluster"
   base_domain            = format("%s.nip.io", replace(module.traefik.external_ip, ".", "-"))
   cluster_issuer         = "ca-issuer"
